4ff822548255f956ad6d2352a0834816e891640c
Rudy Mutter <rudy@yetihq.com>
2014-10-13 20:48:12 -0700

    Update __init__.py

---
+++ event_talker/__init__.py
@@ -0,0 +1,2 @@
+# Need to set up a proper .gitignore and remove local_settings.py, .pyc files, and .idea folder
+# Shouldn't have the 3rd party libraries in your application, should just be able to pip install them
